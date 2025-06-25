fbc8ebf9992811eb5ac39e58bdbc4f87cc54fc5c
Alfan Rozak <93852663+alfanzaky@users.noreply.github.com>
2025-06-25 19:01:37 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,18 @@
+// Efek animasi fade-in saat scroll
+document.addEventListener("DOMContentLoaded", function () {
+  const cards = document.querySelectorAll('.card');
 
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('fade-in');
+      }
+    });
+  }, {
+    threshold: 0.3
+  });
+
+  cards.forEach(card => {
+    observer.observe(card);
+  });
+});
